24aec4d48c504835e8da388f61657658a66f4f20
VerdicAysen <verdicaysen@gmail.com>
2024-05-23 14:08:00 -0500

    Commit

---
+++ Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+ public class Variables
+
+{
+    static void Main()
+
+    {
+        int number = 5;
+        float myFloat = 12.5f;
+        int aValue = 6;
+        Console.WriteLine($"The number {number}, the float {myFloat} and the value {aValue} are all the matters.");
+    }
+}
